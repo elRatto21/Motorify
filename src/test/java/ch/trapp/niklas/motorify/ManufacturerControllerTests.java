55adae04d1b58eb9e6e5cbbabf0b5078379ca059
Niklas Trapp <niklas031712@gmail.com>
2024-04-19 10:46:00 +0200

    implement controller tests

--- src/test/java/ch/trapp/niklas/motorify/ManufacturerControllerTests.java
+++ src/test/java/ch/trapp/niklas/motorify/ManufacturerControllerTests.java
@@ -1,8 +1,8 @@
 package ch.trapp.niklas.motorify;
 
-import ch.trapp.niklas.motorify.repository.BikeRepository;
 import ch.trapp.niklas.motorify.model.Manufacturer;
 import ch.trapp.niklas.motorify.repository.ManufacturerRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.json.JacksonJsonParser;
@@ -18,10 +18,11 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.client.RestTemplate;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -31,56 +32,146 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Rollback(false)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class BikeControllerTests {
+class ManufacturerControllerTests {
 
-	@Autowired
-	private MockMvc api;
+    @Autowired
+    private MockMvc api;
 
-	@Autowired
-	private BikeRepository bikeRepository;
+    @Autowired
+    private ManufacturerRepository manufacturerRepository;
 
-	@Autowired
-	private ManufacturerRepository manufacturerRepository;
+    @BeforeAll
+    void setup() {
+        this.manufacturerRepository.save(new Manufacturer("GetAllTestName", "Germany"));
+        this.manufacturerRepository.save(new Manufacturer("GetTestName", "Switzerland"));
+    }
 
-	@BeforeAll
-	void setup() {
+    @Test
+    void testGetManufacturers() throws Exception {
+        String accessToken = getAccessToken();
 
-	}
+        api.perform(get("/api/manufacturer").header("Authorization", "Bearer " + accessToken)
+                        .with(csrf()))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("GetAllTestName")));
+    }
 
-	@Test
-	void testGetBikes() throws Exception {
-		String accessToken = getAccessToken();
+    @Test
+    void testGetManufacturersByName() throws Exception {
+        String accessToken = getAccessToken();
 
-		api.perform(get("/api/bike").header("Authorization", "Bearer " + accessToken).with(csrf()))
-				.andDo(print()).andExpect(status().isOk());
-	}
+        api.perform(get("/api/manufacturer?name=Get").header("Authorization", "Bearer " + accessToken)
+                        .with(csrf()))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("GetAllTestName")))
+                .andExpect(content().string(containsString("GetTestName")));
+    }
 
-	@Test
-	void testCreateBike() throws Exception {
-		String accessToken = getAccessToken();
+    @Test
+    void testCreateManufacturer() throws Exception {
+        String accessToken = getAccessToken();
 
-		api.perform(post("/api/bike").header("Authorization", "Bearer " + accessToken).with(csrf()))
-				.andDo(print()).andExpect(status().isCreated());
-	}
+        Manufacturer manufacturer = new Manufacturer("Testname", "Testcountry");
 
-	private String getAccessToken() {
-		RestTemplate rest = new RestTemplate();
+        String body = new ObjectMapper().writeValueAsString(manufacturer);
 
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        api.perform(post("/api/manufacturer")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body)
+                        .header("Authorization", "Bearer " + accessToken)
+                        .with(csrf()))
+                .andDo(print()).andExpect(status().isCreated())
+                .andExpect(content().string(containsString("Testname")));
+    }
 
-		String body = "client_id=motorify&" +
-				"grant_type=password&" +
-				"scope=openid profile roles offline_access&" +
-				"username=admin&" +
-				"password=admin";
+    @Test
+    void testDeleteManufacturer() throws Exception {
+        String accessToken = getAccessToken();
 
-		HttpEntity<String> entity = new HttpEntity<>(body, headers);
+        Manufacturer manufacturer = new Manufacturer("NotupdatedName", "Testcountry");
+        Long id = this.manufacturerRepository.save(manufacturer).getId();
 
-		ResponseEntity<String> resp = rest.postForEntity("http://localhost:8080/realms/motorify/protocol/openid-connect/token", entity, String.class);
+        api.perform(delete("/api/manufacturer/" + id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + accessToken)
+                        .with(csrf()))
+                .andDo(print()).andExpect(status().isOk());
+    }
 
-		JacksonJsonParser jsonParser = new JacksonJsonParser();
-		return jsonParser.parseMap(resp.getBody()).get("access_token").toString();
-	}
+    @Test
+    void testUpdateManufacturer() throws Exception {
+        String accessToken = getAccessToken();
+
+        Manufacturer manufacturer = new Manufacturer("NotupdatedName", "Testcountry");
+        Long id = this.manufacturerRepository.save(manufacturer).getId();
+
+        Manufacturer manufacturerUpdated = new Manufacturer("Updatedname", "Testcountry");
+
+        String body = new ObjectMapper().writeValueAsString(manufacturerUpdated);
+
+        api.perform(put("/api/manufacturer/" + id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body)
+                        .header("Authorization", "Bearer " + accessToken)
+                        .with(csrf()))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Updatedname")));
+    }
+
+    @Test
+    void testCreateManufacturerNoPermissions() throws  Exception {
+        String accessToken = getUserAccessToken();
+
+        Manufacturer manufacturer = new Manufacturer("Testname", "Testcountry");
+
+        String body = new ObjectMapper().writeValueAsString(manufacturer);
+
+        api.perform(post("/api/manufacturer")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body)
+                        .header("Authorization", "Bearer " + accessToken)
+                        .with(csrf()))
+                .andDo(print()).andExpect(status().isForbidden());
+    }
+
+    private String getAccessToken() {
+        RestTemplate rest = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        String body = "client_id=motorify&" +
+                "grant_type=password&" +
+                "scope=openid profile roles offline_access&" +
+                "username=admin&" +
+                "password=admin";
+
+        HttpEntity<String> entity = new HttpEntity<>(body, headers);
+
+        ResponseEntity<String> resp = rest.postForEntity("http://localhost:8080/realms/motorify/protocol/openid-connect/token", entity, String.class);
+
+        JacksonJsonParser jsonParser = new JacksonJsonParser();
+        return jsonParser.parseMap(resp.getBody()).get("access_token").toString();
+    }
+
+    private String getUserAccessToken() {
+        RestTemplate rest = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        String body = "client_id=motorify&" +
+                "grant_type=password&" +
+                "scope=openid profile roles offline_access&" +
+                "username=user&" +
+                "password=user";
+
+        HttpEntity<String> entity = new HttpEntity<>(body, headers);
+
+        ResponseEntity<String> resp = rest.postForEntity("http://localhost:8080/realms/motorify/protocol/openid-connect/token", entity, String.class);
+
+        JacksonJsonParser jsonParser = new JacksonJsonParser();
+        return jsonParser.parseMap(resp.getBody()).get("access_token").toString();
+    }
 
 }
